5734a9982606a5f7119db75e36b34e51ec054c4f
Hololens Dev Pc <bitter.daniel@gmail.com>
2019-10-24 20:26:53 -0500

    kafka_2.12-2.3.0

---
+++ startServer.bat
@@ -0,0 +1,9 @@
+D:
+cd D:\Downloads\Installs\kafka\kafka_2.12-2.3.0
+start cmd /k bin\windows\zookeeper-server-start.bat config/zookeeper.properties
+start cmd /k bin\windows\kafka-server-start.bat config/server.properties
+timeout /t 15 /nobreak > NUL
+rem bin\windows\kafka-topics.bat --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic testTopicName
+rem bin\windows\kafka-topics.bat --list --bootstrap-server localhost:9092
+start cmd /k bin\windows\kafka-console-producer.bat --broker-list localhost:9092 --topic testTopicName
+start cmd /k bin\windows\kafka-console-consumer.bat --bootstrap-server localhost:9092 --topic testTopicName --from-beginning
